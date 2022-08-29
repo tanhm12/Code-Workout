4d0a2806daebea58f4dcb82a8909ce6cca4036a4
Zayt02 <zaytpotter@gmail.com>
2022-08-29 10:22:51 +0700

    add count numIslands with dfs and O(1) additional space

---
+++ Leetcode/200.py
@@ -0,0 +1,38 @@
+from typing import List
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]):
+        n, m = len(grid), len(grid[0])
+        def travel(i, j, current_island):
+            if grid[i][j] == "1":
+                grid[i][j] = current_island
+                for ni, nj in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                    if 0 <= ni < n and 0 <= nj < m:
+                        travel(ni, nj, current_island)
+        
+        num_island = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1":
+                    num_island += 1
+                    travel(i, j, num_island+1)
+        
+        return num_island
+    
+    
+grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+
+grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+
+print(Solution().numIslands(grid))
+        
