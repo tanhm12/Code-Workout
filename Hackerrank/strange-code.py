73e0950e374c5d462887941397f54d9c567299a6
Zayt02 <zaytpotter@gmail.com>
2022-09-27 09:16:19 +0700

    add 2 strange-code versions  with benchmark

--- Hackerrank/strange-code.py
+++ Hackerrank/strange-code.py
@@ -1,5 +1,31 @@
+
 import math
 
-def strangeCounter(t):
+def strangeCounter_log(t):
     n = int(math.log((t-1) / 3 + 1, 2))
-    return 3* 2**n - (t - 3*(2**n-1) - 1)
+    x = 2**n
+    return 3* x - (t - 3*(x-1) - 1)
+
+
+def strangeCounter(t):
+    def find_key_num(n):
+        key = 1
+        mul = 3
+        while True:
+            if key + mul <= n:
+                key = key + mul
+            else:
+                break
+            mul *= 2
+        return key
+
+    key = find_key_num(t)
+    counter = key + 2
+    return counter - (t - key)
+
+import numpy as np
+import timeit
+
+t = np.random.randint(int(1e8), int(1e16), (10000,))
+print(timeit.timeit(lambda: [strangeCounter_log(i) for i in t], number=10))
+print(timeit.timeit(lambda: [strangeCounter(i) for i in t], number=10))
