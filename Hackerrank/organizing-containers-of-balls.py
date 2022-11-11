6a02027d25496ae93ae8569ae809f5278b60847c
Tan Hoang <zaytpotter@gmail.com>
2022-11-11 17:31:53 +0700

    add organizing-containers-of-balls  with O(n^2)

---
+++ Hackerrank/organizing-containers-of-balls.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'organizingContainers' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts 2D_INTEGER_ARRAY container as parameter.
+#
+
+def organizingContainers(container):
+    from collections import Counter
+    types = [sum([container[i][j] for i in range(n)]) for j in range(n)]
+    cols = [sum([container[i][j] for j in range(n)]) for i in range(n)]
+    if Counter(types) == Counter(cols):
+        return "Possible"
+    return "Impossible"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        container = []
+
+        for _ in range(n):
+            container.append(list(map(int, input().rstrip().split())))
+
+        result = organizingContainers(container)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
