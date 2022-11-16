e48b0be6608a9ddbb5b25a006bd4f50fff56a4d1
Tan Hoang <zaytpotter@gmail.com>
2022-11-16 17:27:30 +0700

    count bits from 0 to n in O(n)

--- Leetcode/338.py
+++ Leetcode/338.py
@@ -2,18 +2,17 @@ from typing import List
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        if n == 0:
+            return [0]
         sol = [0 for _ in range(n+1)]
         sol[1] = 1
-        for i in range(2, n+1):
-            exact_idx = i - 1
-            if exact_idx % 2 == 0:
-                exact_idx -= 2
-                parent_idx = exact_idx // 2 + 1
-                sol[i] = sol[parent_idx] + 1
-            else:
-                exact_idx -= 1
-                parent_idx = exact_idx // 2 + 1
-                sol[i] = sol[parent_idx]
+        for i in range(1, n//2+1):
+            left = 2*(i-1) + 1
+            right = left + 1
+            if left <= n:
+                sol[left] = sol[i]
+            if right <= n:
+                sol[right] = sol[i] + 1
         
         return sol
 
