f453fb3e69cdb42887a2e5a9fad4ecb265dcd8c8
Tan Hoang <zaytpotter@gmail.com>
2022-12-02 17:49:45 +0700

    threesum without set

--- Leetcode/15.py
+++ Leetcode/15.py
@@ -5,8 +5,9 @@ class Solution:
         import bisect
         nums.sort()
         res = []
-        marks = set()
         for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
             remain = 0 - nums[i]
             fp = i + 1
             sp = len(nums)
@@ -14,24 +15,20 @@ class Solution:
             while fp < sp:
                 tgt = remain - nums[fp]
                 sp = bisect.bisect_left(nums, tgt, lo=fp+1, hi=sp)
-                # print(i, fp, nums[fp],sp, tgt)
                 if sp < len(nums) and nums[sp] == tgt:
-                    triplet_str = ";".join(list(map(str, sorted([nums[i], nums[fp], nums[sp]]))))
-                    if triplet_str not in marks:
-                        marks.add(triplet_str)
-                        res.append([nums[i], nums[fp], nums[sp]])
+                    res.append([nums[i], nums[fp], nums[sp]])
                 fp = bisect.bisect(nums, nums[fp], lo=fp)
                 
         return res
 
 
 nums = [0,0,0]
-nums = [-1,0,1,2,-1,-4]
+# nums = [-1,0,1,2,-1,-4]
 # nums = [0, 1, 1]
 # nums = [0,0,0,0]
 # nums = [0] * 3000
 # nums = [1,-1,-1,0]
-# nums = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
+nums = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
 
 print(Solution().threeSum(nums))
 # print(nums)
