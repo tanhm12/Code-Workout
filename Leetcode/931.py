2536528159fc4d67e1134af2f1ad40283b141288
Tan Hoang <zaytpotter@gmail.com>
2022-12-13 10:50:13 +0700

    min falling path sum O(n^2)

--- Leetcode/931.py
+++ Leetcode/931.py
@@ -4,14 +4,12 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
         # check = lambda i, j:  0<=i<n and 0<=j<n
-        res = 100 *n *n
         for i in range(1, n):
             for j in range(n):
-                prev_ids = ((i-1, j))
+                prev_ids = [(i-1, j)]
                 if j > 0:
                     prev_ids.append((i-1, j-1))
                 if j < n-1:
                     prev_ids.append((i-1, j+1))
                 matrix[i][j] += min([matrix[p0][p1] for p0, p1 in prev_ids])
-                res = min(matrix[i][j], res)
-        return res   
+        return min(matrix[-1])   
