f31e00370c82a1010ae1392431750694ca0f32d7
Tan Hoang <zaytpotter@gmail.com>
2023-03-15 17:00:47 +0700

    minimum deleteions to make string like 0000011111 in O(n)

---
+++ Leetcode/1653.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        ca = s.count("a")
+        res = ca
+        cb = 0
+        for c in s:
+            if c == "b":
+                cb += 1
+            else:
+                ca -= 1
+            res = min(res, ca + cb)
+
+        return res
+s = "aababbab"
+        
+        
