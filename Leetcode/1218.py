0d5621e5979b8c32c2b95c54c9b8b763b8843b77
Tan Hoang <zaytpotter@gmail.com>
2023-07-14 17:48:31 +0700

    longestSubsequence in O(n), simplified

--- Leetcode/1218.py
+++ Leetcode/1218.py
@@ -3,24 +3,15 @@ import bisect
 
 class Solution:
     def longestSubsequence(self, arr: List[int], difference: int) -> int:
-        n = len(arr)
-        all_pos = {}
-        for i, j in enumerate(arr):
-            if j in all_pos:
-                all_pos[j].append(i)
-            else:
-                all_pos[j] = [i]
+        res = 0
+        dp = {}
         
-        res = [1] * n
-        cur_pos = {i: 0 for i in all_pos}
         for i, num in enumerate(arr):
             prev = num - difference
-            if prev in all_pos:
-                while cur_pos[prev] < len(all_pos[prev]) and all_pos[prev][cur_pos[prev]] < i:
-                    cur_pos[prev] += 1
-                if cur_pos[prev] == 0:
-                    continue
-    
-                res[i] = res[all_pos[prev][cur_pos[prev]-1]] + 1
+            if prev in dp:
+                dp[num] = dp[prev] + 1
+            else:
+                dp[num] = 1
+            res = max(res, dp[num])
         
-        return max(res)
+        return res
